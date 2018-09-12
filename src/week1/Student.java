7660e866728f940cec2916a12d25db064f250bef
LeeHieu199 <38397539+LeeHieu199@users.noreply.github.com>
2018-09-12 16:41:45 +0700

    Add files via upload

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,6 +1,29 @@
 package week1;
 
 public class Student {
+    private String name;
+    private String id;
+    private String group;
+    private String email;
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getId(){return id;}
+    public void setId(String id){this.id = id;}
+
+    public String getGroup(){return  group;}
+    public void setGroup(String group){this.group = group;}
+
+    public  String getEmail(){return email;}
+    public void setEmail(String email)
+    {
+        if(email == null || email.isEmpty())
+        {
+        this.email = "Group không hợp lệ!";
+        }
+    else this.email = email;
+    }
 
     // TODO: khai báo các thuộc tính cho Student
 
@@ -10,6 +33,11 @@ public class Student {
      * Constructor 1
      */
     Student() {
+        this.name = "Student";
+        this.id = "000";
+        this.group = "INT22041";
+        this.email = "uet@vnu.edu.vn";
+
         // TODO:
     }
 
@@ -20,6 +48,11 @@ public class Student {
      * @param em
      */
     Student(String n, String sid, String em) {
+
+        this.name = n;
+        this.id = sid;
+        this.email = em;
+        this.group = "INT22041";
         // TODO:
     }
 
@@ -28,11 +61,18 @@ public class Student {
      * @param s
      */
     Student(Student s) {
+
+        this.name = s.name;
+        this.id = s.id;
+        this.group = s.group;
+        this.email = s.email;
         // TODO:
     }
 
     String getInfo() {
+        String info = name + id + group + email;
+        return info;
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+         // xóa dòng này sau khi cài đặt
     }
 }
