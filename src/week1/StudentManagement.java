db9232c3b4f864902ee9ca9cdd0b80cdfcefcf79
LeeHieu199 <38397539+LeeHieu199@users.noreply.github.com>
2018-09-19 15:02:10 +0700

    i

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,15 +1,21 @@
 package week1;
 
 public class StudentManagement {
+    private ArrayList<Student>
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
 
     public boolean sameGroup(Student s1, Student s2) {
+        String s1 = s1.getGroup;
+        String s2 = s2.getGroup;
+        if (s1.equals(s2)) return true;
         // TODO:
         return false; // xóa dòng này sau khi cài đặt
     }
 
     void studentsByGroup() {
+        int i = 0;
+        int n = students.length
         // TODO:
     }
 
@@ -18,6 +24,28 @@ public class StudentManagement {
     }
 
     public static void main(String[] args) {
+        Student s1 = new Student();
+            s1.setName("Le Van Hieu");
+            s1.setId("17020738");
+            s1.setGroup("INT2204");
+            system.out.println(s1.getName);
+            system.out.println(s1.getInfo);
+        Student s2 = new Student();
+            s2.setName("Ngueyn Van A");
+            s2.setId("17020728");
+            s2.setGroup("INT22041");
+        Student s3 = new Student();
+            s3.setName("Ngueyn Van B");
+            s3.setId("17020718");
+            s3.setGroup("INT22041");
+        Student s4 = new Student();
+            s4.setName("Ngueyn Van C");
+            s4.setId("17020748");
+            s4.setGroup("INT22042");
+
+
+
+
         // TODO: iiiii
     }
 }
