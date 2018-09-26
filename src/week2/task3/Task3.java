0529b543e40c90aa19fa4fa9bf44f9d08304466f
LeeHieu199 <38397539+LeeHieu199@users.noreply.github.com>
2018-09-26 12:02:15 +0700

    up

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -1,7 +1,98 @@
 package week2.task3;
 
 public class Task3 {
-
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
+class Teacher{
+    private String Name;
+    private String Subject;
+    private String Email;
+    public String getName(){
+        return Name;
+    }
+    public void setName(String name){
+        this.Name = name;
+    }
+    public String getSubject(){
+        return Subject;
+    }
+    public void setSubject(String subject){
+        this.Subject = subject;
+    }
+    public String getEmail(){
+        return Email;
+    }
+    public void setEmail(String email){
+        this.Email = email;
+    }
+}
+class Cat{
+    private String Name;
+    private String Color;
+    private Int Weight;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        this.Color = color;
+    }
+
+    public Int getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(Int weight) {
+        this.Weight = weight;
+    }
+    public void Meokeu{System.out.println("Meow Meow");}
+    public boolean Tamthe(){
+        if Color.equals("Tam the") return true;
+        return false;
+    }
+    public
+}
+class Laptop{
+    private String Name;
+    private int Price;
+    private String Thick;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getThick() {
+        return Thick;
+    }
+
+    public void setThick(String thick) {
+        Thick = thick;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+    public boolean canbuy(int money){
+        if (money>= this.Price) return true;
+        return false;
+    }
+    public boolean 
+}
