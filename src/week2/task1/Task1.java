df4deff9ff03bbda08bc922088643f03a215f8d7
LeeHieu199 <38397539+LeeHieu199@users.noreply.github.com>
2018-09-26 09:34:53 +0700

    up

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,31 @@
-package week2.task1;
-
 public class Task1 {
 
     public static int gcd(int a, int b) {
-        // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        // TODO: Tính ước chung lớn nhất của 2 số a,
+        while (a!=b)
+        {
+            if(a>b) a = a - b;
+            else b  =b - a;
+        }
+        return (a);
+    }
+    public static int fibonacci(int n)
+    {
+
+        if (n==1) return  0;
+        if (n==2) return  1;
+        if (n==3) return  1;
+        else return fibonacci(n-1) + fibonacci(n-2);
+
     }
 
-    public static int fibonacci(int n) {
-        // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+
+
+    public static void main(String[] args)
+    {
+        int a=5,b=40;
+        int UCLN = gcd(a,b);
+        System.out.println("uoc chung lon nhat la "+UCLN);
+        System.out.println(+fibonacci(5));
     }
 }
--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,30 +1,72 @@
 package week2.task2;
-
 public class Fraction {
+    private int Numerator;
+    private int Denominator;
+    public int getNumerator() {
+        return this.Numerator;
+    }
+    public void setNumerator(int numerator) {
+        this.Numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return this.Denominator;
+    }
+    public void setDenominator(int denominator) {
+        this.Denominator = denominator;
+    }
 
     // TODO: khai báo các thuộc tính
 
     public Fraction(int numerator, int denominator) {
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
+        this.Numerator = numerator;
+        this.Denominator = denominator;
     }
 
     public Fraction add(Fraction other) {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction ps = new Fraction();
+        ps.Numerator = this.getNumerator() * other.getDenominator() + other.Numerator * this.getDenominator();
+        ps.Denominator = this.getDenominator() * other.getDenominator();
+        return ps;
     }
 
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
+        Fraction ps = new Fraction();
+        ps.Numerator = this.getNumerator() * other.getDenominator() - other.getNumerator() * this.getDenominator();
+        ps.Denominator = this.getDenominator() * other.getDenominator();
         return null;
     }
 
     public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
+        Fraction ps = new Fraction();
+        ps.Numerator = this.getNumerator() * other.getNumerator();
+        ps.Denominator = this.getDenominator() * other.getDenominator();
         return null;
     }
 
     public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
+        Fraction ps = new Fraction();
+        ps.Numerator = this.getNumerator() * other.getDenominator();
+        ps.Denominator = this.getDenominator() * other.getNumerator();
         return null;
     }
+    public boolean equals(Object obj){
+        Fraction ps = (Fraction) obj;
+        float ps1 = ps.getNumerator()/ps.getDenominator();
+        float ps2 = this.getNumerator()/this.getDenominator();
+        return ps1.equals(ps2);
+    }
+    public static void main(String[] args){
+        Fraction ps1 = new Fraction(3,5);
+        Fraction ps2 = new Fraction(7,5);
+        System.out.println(ps1.add(ps1));
+    }
 }
+
+
+
